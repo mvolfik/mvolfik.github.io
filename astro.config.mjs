295e5e949156d803caff403efa4dc92f9fba4c0d
Matěj Volf <mat.volfik@gmail.com>
2022-05-07 12:38:57 +0200

    Install formatter & format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import solid from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid(), sitemap()]
+  integrations: [solid(), sitemap()],
 });
