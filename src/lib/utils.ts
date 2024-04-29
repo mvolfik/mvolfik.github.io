7ce99f390156e74a6957baf857318d07e83d34bb
Matěj Volf <git@mvolfik.com>
2024-04-29 20:17:29 +0200

    Fix utils.intersperse

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 export function intersperse<T, U>(array: Array<T>, sep: U): Array<T | U> {
-  return array.reduce((prev, next) => [
-    ...(Array.isArray(prev) ? prev : [prev]),
-    sep,
-    next,
-  ]);
+  return array.reduce(
+    (prev, next) => (prev.length ? [...prev, sep, next] : [next]),
+    [] as const
+  );
 }
